06e9f69eaf951b2cca6ae9a99ee62b67164d942a
Dave Mullins <DaveMullinsJr@gmail.com>
2014-09-17 22:46:35 -0400

    updated home page image, added sliding javascript function for links

---
+++ app/assets/javascripts/myJavascript.js
@@ -0,0 +1,23 @@
+$("#ourMissionButton").click(function(){
+  $('html, body').animate({
+    scrollTop: $("#ourMission").offset().top
+  }, 1000);
+});
+
+
+$("#ourPlanButton").click(function(){
+  $('html, body').animate({
+    scrollTop: $("#ourPlan").offset().top
+  }, 1000);
+});
+
+
+$("#findADealerButton").click(function(){
+  $('html, body').animate({
+    scrollTop: $("#findDealer").offset().top
+  }, 1000);
+});
+// window.onLoad = userGreeting();
+//   function userGreeting(){
+//       alert('hello there' );
+//   };
